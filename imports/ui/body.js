5e81d3f9ecb38cd4761f32330263e0fc9053e860
laitingyou <1003774479@qq.com>
2018-05-17 18:50:33 +0800

    event

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,9 +1,19 @@
 import { Template } from 'meteor/templating';
 import './body.html';
+import { Tasks } from '../api/task.js';
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks(){
+  	return Tasks.find()
+  },
+  value:12
 });   
+Template.body.events({
+	'input input'(e){
+		console.log(Tasks.find({}))
+		console.log(e.target.value)
+		// Tasks.insert( {
+  //    		 _id: 'my-todo'
+  //  		 });
+  Tasks.update('_id', {$set: e.target.value});
+	}
+})
